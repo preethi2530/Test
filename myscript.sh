0966f6e908b3120b4d9dff58c82586df3d210398
preethi2530 <55887297+preethi2530@users.noreply.github.com>
2022-10-14 14:36:35 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
-
-
-
 function generate_keys(){
-
 sudo rm -r validator_keys
 docker run -it --rm -v $(pwd)/validator_keys:/app/validator_keys public.ecr.aws/n2u0q7l0/depositcli:v2.3.0 new-mnemonic --num_validators=1 --mnemonic_language=english
-
 }
-
 generate_keys
-
 echo -e "Success!"
